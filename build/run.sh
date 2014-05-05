ecbdd2666113be6b90e2b7d40167b0d77a6f2542
pandrew <letters@paulnotcom.se>
2014-05-05 21:25:13 +0200

    build undionly.kpxe

--- build/run.sh
+++ build/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /ipxe/src && make bin/ipxe.iso EMBED=/boot.ipxe 
+cd /ipxe/src && make bin/undionly.kpxe bin/ipxe.iso EMBED=/boot.ipxe 
 cp bin/ipxe.iso /out
+cp bin/undionly.kpxe /out
 md5sum bin/ipxe.iso > /out/ipxe.iso.md5.txt

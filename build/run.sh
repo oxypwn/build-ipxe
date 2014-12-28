670a9c021c205f0308068a763c855b321fb45e54
Paul Liljenberg <liljenberg.paul@gmail.com>
2014-12-29 00:08:49 +0100

    updates
    add undionly.kpxe to gitignore
    git pull from ipxe master
    add build-essential
    fix md5sum search for iso in wrong path

--- build/run.sh
+++ build/run.sh
@@ -2,4 +2,4 @@
 cd /ipxe/src && make bin/undionly.kpxe bin/ipxe.iso EMBED=/boot.ipxe 
 cp bin/ipxe.iso /out
 cp bin/undionly.kpxe /out
-md5sum bin/ipxe.iso > /out/ipxe.iso.md5.txt
+cd bin && md5sum ipxe.iso > /out/ipxe.iso.md5
